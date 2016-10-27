f807fd3c374aa360c6d3181b5f1e0f83862e42ab
mcparlandj <john.mcparland@cgi.com>
2016-10-27 08:40:11 +0100

    Added instructions to install Jenkins and remove old JDKs

--- installCiServer.sh
+++ installCiServer.sh
@@ -5,13 +5,22 @@
 ## Th 27 Oct 2016
 ###############################################################################
 
-# Maven
-sudo apt install maven
+# Remove OpenJDK
+sudo apt-get purge openjdk*
+sudo apt-get autoremove
 
 # Java 8
 sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update
 sudo apt-get install oracle-java8-installer
+echo "export JAVA_HOME=/usr/lib/jvm/java-8-oracle/" >> ${HOME}/.bash_aliases
+
+# Maven
+sudo apt install maven
 
 # Jenkins
+wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt-get update
+sudo apt-get install jenkins
 
